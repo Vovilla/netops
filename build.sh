2eeb3906b3b8e38ccf3fe3f47dc2730d5fea7c7d
Krapovnickiy Vladimir <krapovnitskiy@zimbra.fsonet>
2023-02-20 15:52:09 +0300

    juniper templates added

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd library
 git clone https://github.com/networktocode/ntc-ansible --recursive
+cp -r text_fsm_templates/templates/. ntc-ansible/ntc-templates/templates/
+cat text_fsm_templates/index >> ntc-ansible/ntc-templates/templates/index
 cd ..
-docker build -t netops:latest --no-cache .
+
+docker build -t netops:1.0.7 --no-cache .
